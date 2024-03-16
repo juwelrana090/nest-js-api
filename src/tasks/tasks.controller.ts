5c168e8706a1a4e41dcb195281abe5a84c10cb7d
Dalawer Hossamin Juwel <juwelrana090@gmail.com>
2024-03-17 00:32:43 +0600

    Get Task By Filter add

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,16 +1,26 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTaskFilterDto } from './dto/get-task-filter.dto';
 
 @Controller('tasks')
 export class TasksController {
     constructor(private tasksService: TasksService) { }
 
     @Get()
-    getAllTasks(): Task[] {
-        return this.tasksService.getAllTasks();
+    getTasks(@Query() filterDto: GetTaskFilterDto): Task[] {
+        console.log('filterDto', filterDto);
+        if (Object.keys(filterDto).length) {
+            return this.tasksService.getTaskByFilter(filterDto);
+        } else {
+            return this.tasksService.getAllTasks();
+        }
     }
+
+    // getAllTasks(): Task[] {
+    //     return this.tasksService.getAllTasks();
+    // }
 
     @Get('/:id')
     getTaskById(@Param('id') id: string): Task {
