67e954c701337d157104201538e625b9c5f0d610
Dalawer Hossamin Juwel <juwelrana090@gmail.com>
2024-04-13 16:39:31 +0600

    User sing-up add

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
-import { MongoRepository } from 'typeorm';
 import { UserRepository } from './user.repository';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AuthService {
-    constructor(
-        @InjectRepository(User)
-        private userRepository: UserRepository,
-    ) { };
+    constructor(@InjectRepository(User) private userRepository: Repository<User>,) { }
+    
+    async signUp(authCredentialsDto: AuthCredentialsDto): Promise<User> {
+        const { username, password } = authCredentialsDto;
+        const user = new User();
+        user.username = username;
+        user.password = password;
+        await user.save(); 
+        return user;
+    }
 
-     
 }
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,5 +1,6 @@
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 export class UserRepository extends Repository<User> {
   // get all user
@@ -7,4 +8,13 @@ export class UserRepository extends Repository<User> {
   // get user
   // delete user
   // update user
+  
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<User> {
+    const { username, password } = authCredentialsDto;
+    const user = new User();
+    user.username = username;
+    user.password = password;
+    await user.save();
+    return user;
+  }
 }
